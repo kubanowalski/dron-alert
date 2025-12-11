d6901923874a90a015dffaff705b7abba061969a
kubanowalski <kubanowalski@users.noreply.github.com>
2025-12-11 10:33:36 +0100

    Rozwiązanie problemu niewyświetlania pinezki na mapie zgłoszenia. Pobierana była ikonka z zewnętrznej strony, a teraz przechowujem ją w plikach projektu

--- src/components/Map/MapPicker.js
+++ src/components/Map/MapPicker.js
@@ -8,9 +8,9 @@ import "leaflet/dist/leaflet.css";
 // Naprawiamy domyślne ikony pinezek Leaflet, które nie ładują się poprawnie w Next.js
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-    iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-    iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-    shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+    iconRetinaUrl: "/images/marker-icon-2x.png",
+    iconUrl: "/images/marker-icon.png",
+    shadowUrl: "/images/marker-shadow.png",
 });
 
 /**
--- src/components/Map/MapView.js
+++ src/components/Map/MapView.js
@@ -17,9 +17,9 @@ export default function MapView({ location }) {
         // Fix for Leaflet icons in Next.js
         delete L.Icon.Default.prototype._getIconUrl;
         L.Icon.Default.mergeOptions({
-            iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
-            iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+            iconRetinaUrl: '/images/marker-icon-2x.png',
+            iconUrl: '/images/marker-icon.png',
+            shadowUrl: '/images/marker-shadow.png',
         });
     }, []);
 
